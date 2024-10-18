8b07e1412790be72c9db395c3aac55ded099f154
JackieUltra <jbsands78@gmail.com>
2024-10-18 14:39:04 -0700

    expanded t and rt and expanded unit tests

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -4,10 +4,13 @@ def transcribe(seq: str) -> str:
     """
     Transcribes DNA to RNA by generating the complement sequence with T -> U replacement
     """
+    # Invaild check
+    if any(base not in "ATCG" for base in seq.upper()):
+        raise ValueError("Input invalid character. Only A, T, C, G")
     # Dict of complement base pairing
     complement = {'A' : 'U', 'T' : 'A', 'C' : 'G', 'G' : 'C'}
     # Generate our RNA complement seq
-    rna_seq = ''.join(complement[base] for base in seq)
+    rna_seq = ''.join(complement[base] for base in seq.upper())
     
     return rna_seq
 
@@ -16,6 +19,9 @@ def reverse_transcribe(seq: str) -> str:
     """
     Transcribes DNA to RNA then reverses the strand
     """
+    # Invalid check
+    if any(base not in "ATCG" for base in seq.upper()):
+        raise ValueError("Input invalid character. Only A, T, C, G")
     # Transcribe DNA seq into RNA seq
     rna_seq = seq.replace('T', 'U')
     # Reverse the RNA seq
