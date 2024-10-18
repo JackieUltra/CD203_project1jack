4ff797b63399e83d4ef2033c4552c5ae07dbaed3
JackieUltra <jbsands78@gmail.com>
2024-10-18 12:59:56 -0700

    start of test for t and rt func

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -23,14 +23,33 @@ def test_freebie_transcribe_2():
         
 def test_transcribe():
     """
-    TODO: Write your unit test for the transcribe function here.
+    Unit test for the transcribe function
     """
+    # Test Case 1 RNA to DNA
+    assert transcribe("ATCG") == "UAGC", "TC1 fail, there was a transcribe error"
+    # Test Case 2 Return an empty string
+    assert transcribe("") == "", "TC2 fail, empty string unable to return a empty string"
+    # Test Case 3 Lowercase input
+    assert transcribe("atcg".upper()) == "UAGC", "TC3 fail, lowercase input should return"
+    # Test Case 4 Mixed case input
+    assert transcribe("Atcg".upper()) == "UAGC", "TC4 fail, mixed case input should return"
+    ### DO WE NEED TO WRITE FOR EDGE CASES WE KNOW WILL NOT HAPPEN? ie length, invalid character, repitition
     pass
 
 
 def test_reverse_transcribe():
     """
-    TODO: Write your unit test for the
-    reverse transcribe function here.
+    Unit test for the reverse transcribe function
     """
+    # Test Case 1 DNA to RNA
+    assert reverse_transcribe("T") == "U", "TC1 fail, DNA was not converted to RNA"
+    # Test Case 2 Reverse
+    assert reverse_transcribe("ATCG") == "GCUA", "TC2, fail, there was no reversal of the sequence"
+    # Test Case 3 Return and empty string
+    assert reverse_transcribe("") == "", "TC3 fail, empty string unable to return a empty string"
+    # Test Case 4 Lowercase input
+    assert reverse_transcribe("atcg".upper()) == "GCUA", "TC4 fail, lowercase input should return"
+    # Test Case 5 Mixed case input
+    assert reverse_transcribe("Atcg".upper()) == "GCUA", "TC5 fail, mixed case input should return"
+    ### DO WE NEED TO WRITE FOR EDGE CASES WE KNOW WILL NOT HAPPEN? ie length, invalid character, repitition
     pass
