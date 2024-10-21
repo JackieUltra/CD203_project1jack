e46762ed058c9ddd8a3c2c342d12e5010774fb7d
JackieUltra <jbsands78@gmail.com>
2024-10-21 12:52:45 -0700

    create main fucntion

--- main.py
+++ main.py
@@ -6,24 +6,45 @@ from seqparser import (
 
 def main():
     """
-    TODO: The main function
+    The main function 
     """
     # Create instance of FastaParser
-    aparser = FastaParser('data/test.fa')
+    fasta_parser = FastaParser('data/test.fa')
     # Create instance of FastqParser
+    fastq_parser = FastqParser('data/test.fq')
         
     # For each record of FastaParser, Transcribe the sequence
-    # and print it to console
+    print("Transcribed Fasta Sequences:")
+     # Iterate over individual record in the FASTA file
+    for header, sequence in fasta_parser:
+        transcribed_seq = transcribe(sequence)
+        # print it to console
+        print(f"{header}\n{transcribed_seq}")
        
     # For each record of FastqParser, Transcribe the sequence
-    # and print it to console
+    print("Transcribed Fastq Sequences:")
+    # Iterate over individual record in the FASTQ file
+    for header, sequence, _ in fastq_parser:
+        transcribed_seq = transcribe(sequence)
+        # print it to console
+        print(f"{header}\n{transcribed_seq}")
 
 
     # For each record of FastaParser, Reverse Transcribe the sequence
-    # and print it to console
+    print("Reverse Transcribed Fasta Sequences:")
+     # Iterate over individual record in the FASTA file
+    for header, sequence in fasta_parser:
+        reverse_transcribed_seq = reverse_transcribe(sequence)
+        # print to console
+        print(f"{header}\n{reverse_transcribed_seq}")
        
-    # For each record of FastqParser, Reverse Transcribe the sequence
-    # and print it to console
+    # For each record of FastqParser, Reverse Transcribe the sequence and print it to console
+    print("Reverse Transcribed Fastq Sequences:")
+     # Iterate over individual record in the FASTQ file
+    for header, sequence, _ in fastq_parser:
+        reverse_transcribed_seq = reverse_transcribe(sequence)
+        # print to console
+        print(f"{header}\n{reverse_transcribed_seq}")
 
 
 """
