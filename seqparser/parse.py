6f21942bfa2a8b16a8b098c3626a6cf41a802c5a
JackieUltra <jbsands78@gmail.com>
2024-10-23 20:53:59 -0700

    delete some coments

--- seqparser/parse.py
+++ seqparser/parse.py
@@ -9,15 +9,6 @@ class Parser:
     def __init__(self, filename: str):
         """
         Initialization to be shared by all inherited classes.
-        
-        # Recall that this is where we store baseline attribute of a class. For example:
-            class Cat: 
-                def __init__(self, weight: float, breed: str, food: str):
-                    self.weight = weight
-                    self.breed = breed
-                    self.food = food
-                    
-        # What attributes are we initializing here in Parser? 
         """
         
         self.filename = filename
@@ -34,49 +25,13 @@ class Parser:
         """
         This is an overriding of the Base Class Iterable function. Here, for the purposes of this
         assignment, we are defining how this class and all inherited classes interact with loops.
-
-        # Usage
-
-        ```
-        parser_obj = Parser(filename)
-        for record in parser_obj:
-            # do something
-        ```
-
-        The code above calls `__iter__` and for every record it returns, does something with it.
-
-        You may notice we use the keyword `yield` instead of `return` for this function. This is
-        because our `__iter__` is what is known as a generator function, which generates an
-        output then waits until it is called again to resume. In our case, it just reads in a
-        record, outputs it, then waits to read the next record.
-
-        In comparison, functions with `return` simply restart when they are called again, so we
-        would just be reading from the beginning of the file.
-
-        Generator functions are very useful for many bioinformatic tools where you don't need 
-        everything loaded at once and instead are interested in interacting with the stream 
-        (i.e. you need every value once and won't need it again after you use it). This saves
-        quite a bit of memory, especially when you are working with billions of sequences and don't 
-        need to keep all of them in memory. 
-        
         """
-
-        # The proper way to open a file for reading and writing in python3 is to use the `with` / `as` keywords.
-        # and keep the I/O within the nested code block. This will save you from some really nasty bugs that
-        # sometimes close a file before everything you expect to be written/read is written/read. 
-        # 
-        # the interpretation of the following code is that for the lifetime of the filebuffer 
-        # returned by the `open` function it will be accessible as the variable `f_obj`, of type io.TextIOWrapper
         with open(self.filename, "r") as f_obj:
-            
-            # this loop will run forever or return an error, depending on your implementations of _get_record
-            # but we will leave it up to you to implement the fix! 
-
-            # hint: when reading a file, how do we know when to stop reading? what keyword should we use to stop a loop?
 
             while True:
                 rec = self.get_record(f_obj)
-                if rec is None:  # Stop the loop at the end of the file
+                # Stop the loop at the end of the file
+                if rec is None: 
                     break
                 yield rec
 
